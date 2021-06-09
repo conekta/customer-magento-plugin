dc1692ad5ac2cb4b964a6d58327cafa092c59f21
Juan <juan@serfe.com>
2021-06-09 11:57:18 -0300

    repositoy order created to manipulate order events

--- Controller/Webhook/Index.php
+++ Controller/Webhook/Index.php
@@ -2,10 +2,10 @@
 namespace Conekta\Payments\Controller\Webhook;
 
 use Conekta\Payments\Logger\Logger as ConektaLogger;
+use Conekta\Payments\Model\OrderRepository;
 use Exception;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
@@ -13,10 +13,6 @@ use Magento\Framework\Controller\Result\RawFactory;
 use Magento\Framework\DB\Transaction;
 use Magento\Framework\Json\Helper\Data;
 use Magento\Payment\Model\Method\Logger;
-use Magento\Sales\Api\Data\OrderInterface;
-use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\Email\Sender\InvoiceSender;
-use Magento\Sales\Model\Service\InvoiceService;
 
 use Magento\Framework\App\Request\InvalidRequestException;
 
@@ -24,13 +20,13 @@ class Index extends Action implements CsrfAwareActionInterface
 {
 
     private const EVENT_WEBHOOK_PING = 'webhook_ping';
+    private const EVENT_ORDER_CREATED = 'order.created';
+    private const EVENT_ORDER_PENDING_PAYMENT = 'order.pending_payment';
     private const EVENT_ORDER_PAID = 'order.paid';
     private const EVENT_ORDER_EXPIRED = 'order.expired';
 
     private const HTTP_BAD_REQUEST_CODE = 400;
     private const HTTP_OK_REQUEST_CODE = 200;
-
-    protected $orderInterface;
 
     protected $resultJsonFactory;
 
@@ -38,39 +34,30 @@ class Index extends Action implements CsrfAwareActionInterface
 
     protected $helper;
 
-    protected $invoiceService;
-
-    protected $invoiceSender;
-
-    protected $transaction;
-
     private $logger;
 
     private $_conektaLogger;
+
+    private $conektaOrderRepository;
 
     public function __construct(
         Context $context,
         JsonFactory $resultJsonFactory,
         RawFactory $resultRawFactory,
         Data $helper,
-        OrderInterface $orderInterface,
-        InvoiceService $invoiceService,
-        InvoiceSender $invoiceSender,
-        Transaction $transaction,
         Logger $logger,
-        ConektaLogger $conektaLogger
+        ConektaLogger $conektaLogger,
+        OrderRepository $conektaOrderRepository
+
     ) {
         parent::__construct($context);
+        $this->_conektaLogger = $conektaLogger;
+        $this->_conektaLogger->info('Controller Index :: __construct');
         $this->resultJsonFactory = $resultJsonFactory;
         $this->resultRawFactory = $resultRawFactory;
         $this->helper = $helper;
-        $this->orderInterface = $orderInterface;
-        $this->invoiceService = $invoiceService;
-        $this->invoiceSender = $invoiceSender;
-        $this->transaction = $transaction;
         $this->logger = $logger;
-        $this->_conektaLogger = $conektaLogger;
-        $this->_conektaLogger->info('Controller Index :: __construct');
+        $this->conektaOrderRepository = $conektaOrderRepository;
     }
 
     /** * @inheritDoc */
@@ -107,137 +94,40 @@ class Index extends Action implements CsrfAwareActionInterface
 
         $event = $body['type'];
         
-        switch($event){
-            case self::EVENT_WEBHOOK_PING:
-                $response = self::HTTP_OK_REQUEST_CODE;
-                break;
-            case self::EVENT_ORDER_PAID:
-                $response = $this->orderPaidProcess($body);
-                break;
-            case self::EVENT_ORDER_EXPIRED:
-                $response = $this->orderExpiredProcess($body);
-                break;
+        try{
+
+            $response = self::HTTP_OK_REQUEST_CODE;
+            switch($event){
+                case self::EVENT_WEBHOOK_PING:
+                    $response = self::HTTP_OK_REQUEST_CODE;
+                    break;
+                
+                case self::EVENT_ORDER_CREATED:
+                case self::EVENT_ORDER_PENDING_PAYMENT:
+                    $this->conektaOrderRepository->findByMetadataOrderId($body);
+                    break;
+                
+                case self::EVENT_ORDER_PAID:
+                    $this->conektaOrderRepository->payOrder($body);
+                    break;
+                
+                case self::EVENT_ORDER_EXPIRED:
+                    $this->conektaOrderRepository->expireOrder($body);
+                    break;
+                
+                default:
+                    //If the event not exist, response Bad Request
+                    $response = self::HTTP_BAD_REQUEST_CODE;
+            }
+
+        }catch(Exception $e){
+            $this->_conektaLogger->error('Controller Index :: '. $e->getMessage());
+            $response = self::HTTP_BAD_REQUEST_CODE;
         }
-
-
+        
         return $resultRaw->setHttpResponseCode($response);
         
     }
 
-    private function orderExpiredProcess($body){
-        $this->_conektaLogger->info('Controller Index :: orderExpiredProcess');
-
-        $responseCode = self::HTTP_BAD_REQUEST_CODE;
-        try {
-            if (
-                !isset($body['data']['object']) &&
-                !isset($body['data']['object']['metadata']) &&
-                !isset($body['data']['object']['metadata']['order_id'])
-            ){
-                throw new Exception(_('Missing order information'));
-            }
-            
-            $orderId = $body['data']['object']['metadata']['order_id'];
-            $order = $this->orderInterface->loadByIncrementId($orderId);
-
-            if(!$order->getId()){
-                throw new Exception(_('We could not locate the order in the store'));
-            }
-
-            //Only update order status if is Pending
-            if(
-                $order->getState() === Order::STATE_PENDING_PAYMENT ||
-                $order->getState() === Order::STATE_PAYMENT_REVIEW
-            ){
-                $order->setSate(Order::STATE_CANCELED);
-                $order->setStatus(Order::STATE_CANCELED);
-
-                $order->addStatusHistoryComment("Order Expired")
-                        ->setIsCustomerNotified(true);
-
-                $order->save();
-            }
-            
-            $this->_conektaLogger->info('Controller Index :: orderExpiredProcess: Order Canceled');
-
-            //everything is ok
-            $responseCode = self::HTTP_OK_REQUEST_CODE;
-        }catch(Exception $e){
-            $this->_conektaLogger->error('Controller Index :: orderExpiredProcess: ' . $e->getMessage());
-
-            $responseCode = self::HTTP_BAD_REQUEST_CODE;
-        }
-        
-
-        return $responseCode;
-    }
-
-    private function orderPaidProcess($body){
-
-        if (!isset($body['data']['object'])){
-            return self::HTTP_BAD_REQUEST_CODE;
-        }
-        
-        $charge = $body['data']['object'];
-        if (!isset($charge['payment_status']) || $charge['payment_status'] !== "paid"){
-            return self::HTTP_BAD_REQUEST_CODE;
-        }
-
-        try {
-            $order = $this->orderInterface->loadByIncrementId($charge['metadata']['order_id']);
-            if (!$order->getId()) {
-                $this->_conektaLogger->error(
-                    'Controller Index :: execute - The order does not allow an invoice to be created'
-                );
-                return;
-            }
-            $order->setSate(Order::STATE_PROCESSING);
-            $order->setStatus(Order::STATE_PROCESSING);
-
-            $order->addStatusHistoryComment("Payment received successfully")
-                ->setIsCustomerNotified(true);
-
-            $order->save();
-            $this->_conektaLogger->info('Controller Index :: execute - Order status updated');
-
-            $invoice = $this->invoiceService->prepareInvoice($order);
-            $invoice->register();
-            $invoice->save();
-            $transactionSave = $this->transaction->addObject(
-                $invoice
-            )->addObject(
-                $invoice->getOrder()
-            );
-            $transactionSave->save();
-            $this->_conektaLogger->info('Controller Index :: execute - The invoice to be created');
-
-            try {
-                $this->invoiceSender->send($invoice);
-                $order->addStatusHistoryComment(
-                    __('Notified customer about invoice creation #%1.', $invoice->getId())
-                )
-                    ->setIsCustomerNotified(true)
-                    ->save();
-                $this->_conektaLogger->info(
-                    'Controller Index :: execute - Notified customer about invoice creation'
-                );
-            } catch (\Exception $e) {
-                $this->_conektaLogger->error(
-                    'Controller Index :: execute - We can\'t send the invoice email right now.'
-                );
-                $this->logger->error(__('[Conekta]: We can\'t send the invoice email right now.'));
-            }
-
-            return self::HTTP_OK_REQUEST_CODE;
-        } catch (\Exception $e) {
-            $this->_conektaLogger->critical(
-                'Controller Index :: execute - Error processing webhook notification',
-                ['exception' => $e]
-            );
-            $this->logger->error(__('[Conekta]: Error processing webhook notification'));
-            $this->logger->debug(['exception' => $e]);
-            return self::HTTP_BAD_REQUEST_CODE;
-        }
-
-    }
+    
 }
---
+++ Model/OrderRepository.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Conekta\Payments\Model;
+
+use Conekta\Payments\Logger\Logger as ConektaLogger;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Email\Sender\InvoiceSender;
+use Magento\Sales\Model\Service\InvoiceService;
+use Magento\Framework\DB\Transaction;
+
+class OrderRepository
+{
+    protected $orderInterface;
+
+    protected $invoiceService;
+
+    protected $invoiceSender;
+
+    protected $transaction;
+
+    private $_conektaLogger;
+
+    public function __construct(
+        OrderInterface $orderInterface,
+        InvoiceService $invoiceService,
+        InvoiceSender $invoiceSender,
+        Transaction $transaction,
+        ConektaLogger $conektaLogger
+    ) {
+        $this->orderInterface = $orderInterface;
+        $this->invoiceService = $invoiceService;
+        $this->invoiceSender = $invoiceSender;
+        $this->transaction = $transaction;
+        $this->_conektaLogger = $conektaLogger;
+        $this->_conektaLogger->info('OrderRepository :: __construct');
+    }
+
+
+    /**
+     * Find store order in request body. 
+     * If the keys['data']['object']['metadata']['order_id'] does not exist 
+     * in $body, throws an Exception
+     * 
+     * @param array $body
+     * @return Order
+     * @throws LocalizedException
+     */
+    function findByMetadataOrderId($body){
+        $this->_conektaLogger->info('OrderRepository :: findByMetadataOrderId started');
+
+        if (
+            !isset($body['data']['object']) &&
+            !isset($body['data']['object']['metadata']) &&
+            !isset($body['data']['object']['metadata']['order_id'])
+        ){
+            throw new LocalizedException(__('Missing order information'));
+        }
+        
+        $orderId = $body['data']['object']['metadata']['order_id'];
+        $order = $this->orderInterface->loadByIncrementId($orderId);
+        
+        return $order;
+    }
+
+    /**
+     * Finds order by metadata id passed in $body.
+     * If the state of store order is Pending, set as CANCELED.
+     *
+     * If order not exists, throws an exception
+     * @param array $body
+     * @return void
+     * @throws LocalizedException
+     */
+    function expireOrder($body){
+        $this->_conektaLogger->info('OrderRepository :: expireOrder started');
+
+        $order = $this->findByMetadataOrderId($body);
+
+        if(!$order->getId()){
+            throw new LocalizedException(__('We could not locate the order in the store'));
+        }
+
+        //Only update order status if is Pending
+        if(
+            $order->getState() === Order::STATE_PENDING_PAYMENT ||
+            $order->getState() === Order::STATE_PAYMENT_REVIEW
+        ){
+            $order->setSate(Order::STATE_CANCELED);
+            $order->setStatus(Order::STATE_CANCELED);
+
+            $order->addStatusHistoryComment("Order Expired")
+                    ->setIsCustomerNotified(true);
+
+            $order->save();
+        }
+        
+        $this->_conektaLogger->info('OrderRepository :: orderExpiredProcess: Order has been Canceled');
+    }
+
+    function payOrder($body){
+
+        if (!isset($body['data']['object'])){
+            throw new LocalizedException(__('Missing order information'));
+        }
+        
+        $charge = $body['data']['object'];
+        if (!isset($charge['payment_status']) || $charge['payment_status'] !== "paid"){
+            throw new LocalizedException(__('Missing order information'));
+        }
+
+        $order = $this->orderInterface->loadByIncrementId($charge['metadata']['order_id']);
+        if (!$order->getId()) {
+            $message = 'The order does not allow an invoice to be created';
+            $this->_conektaLogger->error(
+                'OrderRepository :: execute - ' . $message
+            );
+            throw new LocalizedException(__($message));
+        }
+
+        $order->setSate(Order::STATE_PROCESSING);
+        $order->setStatus(Order::STATE_PROCESSING);
+
+        $order->addStatusHistoryComment("Payment received successfully")
+            ->setIsCustomerNotified(true);
+
+        $order->save();
+        $this->_conektaLogger->info('OrderRepository :: execute - Order status updated');
+
+        $invoice = $this->invoiceService->prepareInvoice($order);
+        $invoice->register();
+        $invoice->save();
+        $transactionSave = $this->transaction->addObject(
+            $invoice
+        )->addObject(
+            $invoice->getOrder()
+        );
+        $transactionSave->save();
+        $this->_conektaLogger->info('OrderRepository :: execute - The invoice to be created');
+
+        try {
+            $this->invoiceSender->send($invoice);
+            $order->addStatusHistoryComment(
+                __('Notified customer about invoice creation #%1.', $invoice->getId())
+            )
+                ->setIsCustomerNotified(true)
+                ->save();
+            $this->_conektaLogger->info(
+                'OrderRepository :: execute - Notified customer about invoice creation'
+            );
+        } catch (\Exception $e) {
+            $this->_conektaLogger->error(
+                'OrderRepository :: execute - We can\'t send the invoice email right now.'
+            );
+        }
+
+    }
+}
