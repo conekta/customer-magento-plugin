cd0cb12395644be0a641130bd7735b9bca957728
Juan <juan@serfe.com>
2021-08-05 13:45:02 -0300

    0087608 - sanitization of phones, streets, names and postal codes

--- Exception/ConektaException.php
+++ Exception/ConektaException.php
@@ -6,5 +6,8 @@ use Magento\Framework\Exception\LocalizedException;
 
 class ConektaException extends LocalizedException
 {
-    
+    public const INVALID_PHONE_MESSAGE = 'Télefono no válido. 
+        El télefono debe tener al menos 10 carácteres.
+        Los caracteres especiales se desestimaran, solo se puede ingresar como 
+        primer carácter especial: +';
 }
