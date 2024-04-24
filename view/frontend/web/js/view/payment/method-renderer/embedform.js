54fe3c27889907f4fbca83abcc5e1dae1e9facba
Franklin <franklin.carrero@conekta.com>
2024-04-24 16:19:33 -0500

    remove dedicate error

--- view/frontend/web/js/view/payment/method-renderer/embedform.js
+++ view/frontend/web/js/view/payment/method-renderer/embedform.js
@@ -295,7 +295,6 @@ define(
                         console.error(error);
                     },
                     onFinalizePayment: function (event) {
-                        return ;
                         self.iframOrderData(event);
                         self.beforePlaceOrder();
                     },
