a79743e1a020570e5844f58dfd7f230b7673f0b6
Franklin <franklin.carrero@conekta.com>
2025-03-26 13:02:38 -0500

    remove intencional error

--- view/frontend/web/js/view/payment/method-renderer/embedform.js
+++ view/frontend/web/js/view/payment/method-renderer/embedform.js
@@ -297,7 +297,6 @@ define(
                             console.error(error);
                         },
                         onFinalizePayment: function (event) {
-                            return;
                             self.iframOrderData(event);
                             self.beforePlaceOrder();
                         },
