bad059047b766c58e939792a09a48705f89b7655
Martín Agüera <maguera@conekta.com>
2025-11-12 11:43:14 -0300

    feat: [AI-491] - implemented new classes for BNPL payment handling, including transaction authorization, request building, response processing, and configuration provider

---
+++ Gateway/Http/Client/Bnpl/TransactionAuthorize.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Conekta\Payments\Gateway\Http\Client\Bnpl;
+
+use Conekta\Payments\Api\ConektaApiClient;
+use Conekta\Payments\Helper\Data as ConektaHelper;
+use Conekta\Payments\Logger\Logger as ConektaLogger;
+use Conekta\Payments\Api\Data\ConektaSalesOrderInterface;
+use Conekta\Payments\Model\ConektaSalesOrderFactory;
+use Magento\Payment\Gateway\Http\ClientInterface;
+use Magento\Payment\Gateway\Http\TransferInterface;
+use Magento\Payment\Model\Method\Logger;
+
+class TransactionAuthorize implements ClientInterface
+{
+    const SUCCESS = 1;
+    const FAILURE = 0;
+
+    /**
+     * @var array
+     */
+    private array $results = [
+        self::SUCCESS,
+        self::FAILURE
+    ];
+
+    /**
+     * @var Logger
+     */
+    private Logger $logger;
+
+    protected ConektaHelper $_conektaHelper;
+
+    private ConektaLogger $_conektaLogger;
+
+    protected ConektaSalesOrderFactory $conektaSalesOrderFactory;
+
+    /**
+     * @var ConektaApiClient
+     */
+    private ConektaApiClient $conektaApiClient;
+
+    /**
+     * @param Logger $logger
+     * @param ConektaHelper $conektaHelper
+     * @param ConektaLogger $conektaLogger
+     * @param ConektaApiClient $conektaApiClient
+     * @param ConektaSalesOrderFactory $conektaSalesOrderFactory
+     */
+    public function __construct(
+        Logger                   $logger,
+        ConektaHelper            $conektaHelper,
+        ConektaLogger            $conektaLogger,
+        ConektaApiClient         $conektaApiClient,
+        ConektaSalesOrderFactory $conektaSalesOrderFactory
+    )
+    {
+        $this->_conektaHelper = $conektaHelper;
+        $this->_conektaLogger = $conektaLogger;
+        $this->conektaApiClient = $conektaApiClient;
+        $this->_conektaLogger->info('HTTP Client Bnpl TransactionAuthorize :: __construct');
+        $this->logger = $logger;
+        $this->conektaSalesOrderFactory = $conektaSalesOrderFactory;
+    }
+
+    /**
+     * Places request to gateway. Returns result as ENV array
+     *
+     * @param TransferInterface $transferObject
+     * @return array
+     * @throws \Exception
+     */
+    public function placeRequest(TransferInterface $transferObject)
+    {
+        $this->_conektaLogger->info('HTTP Client Bnpl TransactionAuthorize :: placeRequest');
+        $request = $transferObject->getBody();
+
+        $orderParams['currency'] = $request['CURRENCY'];
+        $orderParams['line_items'] = $request['line_items'];
+        $orderParams['tax_lines'] = $request['tax_lines'];
+        $orderParams['customer_info'] = $request['customer_info'];
+        $orderParams['discount_lines'] = $request['discount_lines'];
+        if (!empty($request['shipping_lines'])) {
+            $orderParams['shipping_lines'] = $request['shipping_lines'];
+        }
+        if (!empty($request['shipping_contact'])) {
+            $orderParams['shipping_contact'] = $request['shipping_contact'];
+        }
+        $orderParams['metadata'] = $request['metadata'];
+        $chargeParams = $request['payment_method_details'];
+
+        $result_code = '';
+        $txn_id = '';
+        $ord_id = '';
+        $error_code = '';
+
+        try {
+            $conektaOrder = $this->conektaApiClient->createOrder($orderParams);
+            $charge = $this->conektaApiClient->createOrderCharge($conektaOrder->getId(), $chargeParams);
+
+            if ($charge->getId() && $conektaOrder->getId()) {
+                $result_code = 1;
+                $txn_id = $charge->getId();
+                $ord_id = $conektaOrder->getId();
+
+                $this->conektaSalesOrderFactory
+                    ->create()
+                    ->setData([
+                        ConektaSalesOrderInterface::CONEKTA_ORDER_ID => $ord_id,
+                        ConektaSalesOrderInterface::INCREMENT_ORDER_ID => $orderParams['metadata']['order_id']
+                    ])
+                    ->save();
+            } else {
+                $result_code = 666;
+            }
+        } catch (\Exception $e) {
+            $this->_conektaLogger->error(__('[Conekta]: Payment capturing error.'));
+            $this->logger->debug(
+                [
+                    'request' => $request,
+                    'response' => $e->getMessage()
+                ]
+            );
+            $this->_conektaLogger->info(
+                'HTTP Client Bnpl TransactionAuthorize :: placeRequest: Payment authorize error ' . $e->getMessage()
+            );
+            throw new \Exception(__($e->getMessage()));
+        }
+
+        $response = $this->generateResponseForCode(
+            $result_code,
+            $txn_id,
+            $ord_id
+        );
+
+        // Store BNPL specific data
+        $paymentMethod = $charge->getPaymentMethod();
+        $response['offline_info'] = [
+            "type" => $paymentMethod->getType(),
+            "data" => [
+                "reference" => $paymentMethod->getReference() ?? '',
+                "expires_at" => $paymentMethod->getExpiresAt() ?? '',
+                "payment_url" => $paymentMethod->getPaymentUrl() ?? '',
+                "monthly_installments" => $paymentMethod->getMonthlyInstallments() ?? '',
+                "status" => $charge->getStatus() ?? ''
+            ]
+        ];
+
+        $response['error_code'] = $error_code;
+
+        $this->logger->debug(
+            [
+                'request' => $request,
+                'response' => $response
+            ]
+        );
+
+        $this->_conektaLogger->info(
+            'HTTP Client Bnpl TransactionAuthorize :: placeRequest',
+            [
+                'request' => $request,
+                'response' => $response
+            ]
+        );
+
+        $response['payment_method_details'] = $request['payment_method_details'];
+
+        return $response;
+    }
+
+    protected function generateResponseForCode($resultCode, $txn_id, $ord_id): array
+    {
+        $this->_conektaLogger->info('HTTP Client Bnpl TransactionAuthorize :: generateResponseForCode');
+
+        return array_merge(
+            [
+                'RESULT_CODE' => $resultCode,
+                'TXN_ID' => $txn_id,
+                'ORD_ID' => $ord_id
+            ]
+        );
+    }
+}
+
---
+++ Gateway/Request/Bnpl/AuthorizeRequest.php
@@ -0,0 +1,72 @@
+<?php
+namespace Conekta\Payments\Gateway\Request\Bnpl;
+
+use Conekta\Payments\Helper\Data as ConektaHelper;
+use Conekta\Payments\Logger\Logger as ConektaLogger;
+use Magento\Payment\Gateway\ConfigInterface;
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Request\BuilderInterface;
+
+class AuthorizeRequest implements BuilderInterface
+{
+    private $config;
+
+    private $subjectReader;
+
+    protected $_conektaHelper;
+
+    private $_conektaLogger;
+
+    public function __construct(
+        ConfigInterface $config,
+        SubjectReader $subjectReader,
+        ConektaHelper $conektaHelper,
+        ConektaLogger $conektaLogger
+    ) {
+        $this->_conektaHelper = $conektaHelper;
+        $this->_conektaLogger = $conektaLogger;
+        $this->_conektaLogger->info('Request Bnpl AuthorizeRequest :: __construct');
+
+        $this->config = $config;
+        $this->subjectReader = $subjectReader;
+    }
+
+    public function build(array $buildSubject)
+    {
+        $this->_conektaLogger->info('Request Bnpl AuthorizeRequest :: build');
+
+        $paymentDO = $this->subjectReader->readPayment($buildSubject);
+        $payment = $paymentDO->getPayment();
+        $order = $paymentDO->getOrder();
+        
+        $expiry_date = $this->_conektaHelper->getExpiredAt();
+        $amount = $this->_conektaHelper->convertToApiPrice($order->getGrandTotalAmount());
+
+        $request['metadata'] = [
+            'plugin' => 'Magento',
+            'plugin_version' => $this->_conektaHelper->getMageVersion(),
+            'plugin_conekta_version' => $this->_conektaHelper->pluginVersion(),
+            'order_id'       => $order->getOrderIncrementId(),
+            'soft_validations'  => 'true'
+        ];
+
+        $request['payment_method_details'] = $this->getChargeBnpl($amount, $expiry_date);
+        $request['CURRENCY'] = $order->getCurrencyCode();
+        $request['TXN_TYPE'] = 'A';
+
+        return $request;
+    }
+
+    public function getChargeBnpl($amount, $expiry_date)
+    {
+        $charge = [
+            'payment_method' => [
+                'type' => 'bnpl',
+                'expires_at' => $expiry_date
+            ],
+            'amount' => $amount
+        ];
+        return $charge;
+    }
+}
+
